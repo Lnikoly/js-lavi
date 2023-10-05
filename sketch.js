9ff3097407332749cde0ac36f83e940934d8a849
Lnikoly <146379017+Lnikoly@users.noreply.github.com>
2023-10-05 16:19:13 -0300

    adcionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+}
+
+function draw() { 
+    stroke("blue");
+FileList("red");
+
+if(mouseIspressed){
+    Reflect(mouseX, mouseY, 20, 35);
+}
+
+}
